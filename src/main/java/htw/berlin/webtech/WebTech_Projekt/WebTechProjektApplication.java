c19e3808a61acf876b338c0127b5891b71b90166
sewism <adrian.j.franz@gmail.com>
2021-10-07 16:57:48 +0200

    first commit

---
+++ src/main/java/htw/berlin/webtech/WebTech_Projekt/WebTechProjektApplication.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtech.WebTech_Projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTechProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTechProjektApplication.class, args);
+	}
+
+}
