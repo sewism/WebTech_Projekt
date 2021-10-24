2b0296f90834549c460ffe4bb73c29586009faa8
sewism <adrian.j.franz@gmail.com>
2021-10-24 15:32:28 +0200

    1. Milestone

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# WebTech_Projekt
-# WebTech_Projekt
+#WebTech_Projekt
+
+##Thema: Shareconomy-Plattform
+Von Adrian Franz
+
+###Überblick
+Ich möchte eine Website erstellen, auf der Nutzer ihre persönlichen
+Gegenstände zum Verleih anbieten können.
+Das Projekt wird von der Funktionalität zuerst stark an eBay-Kleinanzeigen 
+erinnern. Ich plane darüber hinaus jedoch weitere Features zu implementieren.
+
+###Notwendige Features:
+
+- [ ] Registrieren/Einlogen
+- [ ] Item erstellen
+- [ ] Items anzeigen lassen
+- [ ] Nach Kategorie sortieren
+
+
+
+###Zusätzliche Features
+
+- Foto hochladen
+- Umkreissuche
+- Nachrichten
+- Benutzerprofil
+- Business
+
--- src/main/java/htw/berlin/webtech/WebTech_Projekt/helloworld.java
+++ src/main/java/htw/berlin/webtech/WebTech_Projekt/helloworld.java
@@ -3,15 +3,14 @@ package htw.berlin.webtech.WebTech_Projekt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class helloworld {
 
-    @GetMapping("/helloworld")
-    public String hellowWorld(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "helloworld";
+    @GetMapping("/")
+    public ModelAndView helloWorld(){
+        return new ModelAndView("helloworld") ;
     }
 
 }
